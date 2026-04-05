cd0c745a825541503c3d268b3bbdbe86ae014b39
kavitagodge45 <kavitagodge1234@gmail.com>
2026-04-05 15:49:00 +0530

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -7,7 +7,7 @@ guessed_letters = []
 wrong_guesses = 0
 max_attempts = 6
 
-print("🎮 Welcome to Hangman Game!")
+print("Welcome to Hangman Game!")
 
 while wrong_guesses < max_attempts:
     display_word = ""
@@ -22,20 +22,20 @@ while wrong_guesses < max_attempts:
     guess = input("Enter a letter: ").lower()
 
     if guess in guessed_letters:
-        print("⚠️ Already guessed!")
+        print("Already guessed!")
         continue
 
     guessed_letters.append(guess)
 
     if guess in word:
-        print("✅ Correct!")
+        print("Correct!")
     else:
         wrong_guesses += 1
-        print(f"❌ Wrong! Attempts left: {max_attempts - wrong_guesses}")
+        print(f"Wrong! Attempts left: {max_attempts - wrong_guesses}")
 
     if "_" not in display_word:
-        print("\n🎉 You Won! The word was:", word)
+        print("\n You Won! The word was:", word)
         break
 
 if wrong_guesses == max_attempts:
-    print("\n💀 You Lost! The word was:", word)hangman.pyhangman.py
+    print("\n You Lost! The word was:", word)hangman.pyhangman.py
